3ce889fe011f2385d066b7ca880cc9ed24d2b92b
Sean O <seanosaur@me.com>
2014-06-25 13:35:21 -0500

    Incorporating theme, add rails_12factor gem

--- Gemfile
+++ Gemfile
@@ -28,6 +28,10 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 
 gem 'sorcery'
 
+gem 'bootstrap-sass', '~> 3.1.1'
+
+gem 'rails_12factor'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module F2ydRails
   class Application < Rails::Application
+
+    config.assets.paths << Rails.root.join("app", "assets", "fonts", "images", "javascripts", "stylesheets")
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
