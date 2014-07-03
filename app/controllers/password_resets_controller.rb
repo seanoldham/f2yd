f9aea0f952cb341fe1f204f26049a1dd079fd30c
Sean O <seanosaur@me.com>
2014-07-03 10:36:21 -0500

    new approach for mailer

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,13 +1,10 @@
 class PasswordResetsController < ApplicationController
 	skip_before_filter :require_login
 
-  def reset_password_email_sent_at
-  end
-
   def create
   	@user = User.find_by_email(params[:email])
   	@user.deliver_reset_password_instructions! if @user
-  	redirect_to("/", :notice => 'Instructions have been sent to your email.')
+  	redirect_to root_url, :notice => 'Instructions have been sent to your email.'
   end
 
   def edit
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.configure do
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.smtp_settings = {
+  enable_starttls_auto: true,
+  address: 'box426.bluehost.com',
+  port: 465,
+  authentication: 'plain',
+  user_name: 'admin@formstoyourdoor.com',
+  password: 'LN8KgFDfM98D'
+}
+end
