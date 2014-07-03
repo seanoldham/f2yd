d1826ad5b4edb717893ddfc807c64f8147f7fad7
Sean O <seanosaur@me.com>
2014-07-03 08:25:50 -0500

    changed root_path to "/"

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,40 +3,27 @@ class PasswordResetsController < ApplicationController
 
   def create
   	@user = User.find_by_email(params[:email])
-
   	@user.deliver_reset_password_instructions! if @user
-
-  	redirect_to(root_path, :notice => 'Instructions have been sent to your email.')
+  	redirect_to("/", :notice => 'Instructions have been sent to your email.')
   end
 
   def edit
   	@user = User.load_from_reset_password_token(params[:id])
   	@token = parmas[:id]
-
-  	if @user.blank?
-  		not_authenticated
-  		return
-  	end
+  	not_authenticated if !@user
   end
 
   def update
   	@token = params[:token]
-  	@user = User.load_from_reset_password_token(params[:token])
-
-  	if @user.blank?
-  		not_authenticated
-  		return
-  	end
-
+  	@user = User.load_from_reset_password_token(@token)
+  	not_authenticated if !@user
   	@user.password_confirmation = parms[:user][:password_confirmation]
 
   	if @user.change_password!(params[:user][:password])
-  		redirect_to(root_path, :notice => 'Password was successfully updated.')
+  		redirect_to("/", :notice => 'Password was successfully updated.')
   	else
   		render :action => 'edit'
   	end
   end
 
-  def reset_password_email_sent_at
-  end
 end
