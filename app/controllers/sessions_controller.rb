4c3341b8bd0b2214e3bf0109e8dce07cb8a29a05
Sean O <seanosaur@me.com>
2014-08-01 20:44:53 -0500

    if login error, redirect back to login page and show notice

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
   	if user
   		redirect_to root_url, :notice => "Logged in!"
   	else
-  		flash.now.alert = "Email or password was invalid."
+  		redirect_to login_url, :notice => "Email or password was invalid."
   	end
   end
 
