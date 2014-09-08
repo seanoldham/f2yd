649b96a08b41bec61055cc3fd0ce90d84e3b794f
Sean O <seanosaur@me.com>
2014-09-08 16:23:23 -0500

    more cleanup, add day and form type helpers

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,21 @@
 module CoursesHelper
+	def days_of_week
+    [
+      ['Monday', 'M'],
+      ['Tuesday', 'T'],
+      ['Wednesday', 'W'],
+      ['Thursday', 'Tr'],
+      ['Friday', 'F'],
+      ['Saturday', 'Sa'],
+      ['Sunday', 'Su']
+    ]
+	end
+
+	def form_types
+		[
+			['Green Scantron', 'Green Scantron'],
+			['Blue Scantron', 'Blue Scantron'],
+			['Blue Book', 'Blue Book']
+		]
+	end
 end
