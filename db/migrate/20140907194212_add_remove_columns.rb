c8be8f5481817e072ac624f3dc53e6bb16e05dd1
Sean O <seanosaur@me.com>
2014-09-08 19:59:08 -0500

    delete non-working migration files

--- db/migrate/20140907194212_add_remove_columns.rb
+++
@@ -1,7 +0,0 @@
-class AddRemoveColumns < ActiveRecord::Migration
-  def change
-  	remove_column :courses, :exam_date
-  	add_column :courses, :exam_date, :string
-    add_column :courses, :meet_time, :string
-  end
-end
