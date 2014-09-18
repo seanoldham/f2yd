a289804871931f85f0307783c899e5294cc31c75
Sean O <seanosaur@me.com>
2014-09-18 16:46:01 -0500

    delete railsview folder - missed when removing gemfile previously

--- lib/railsview/themes/f2yd/application/app/controllers/application_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  private
-  def not_authenticated
-  	redirect_to login_url, :alert => "First login to access this page."
-  end
-end
